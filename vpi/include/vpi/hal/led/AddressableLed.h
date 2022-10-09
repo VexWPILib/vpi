8e12cc33d35eae596c79ea33e6151822eb6c9eb8
dcieslak19973 <dcieslak@hotmail.com>
2022-10-08 23:58:35 -0500

    Fix namespace

--- vpi/include/vpi/hal/led/AddressableLed.h
+++ vpi/include/vpi/hal/led/AddressableLed.h
@@ -13,81 +13,86 @@
 // Based on https://github.com/jpearman/V5_addr_led_demo/blob/master/include/vex_addrled.h
 #include "vex.h"
 
+#include <cmath>
+
 extern "C" {
   // private addressable LED API
   int32_t  vexAdiAddrLedSet( uint32_t index, uint32_t port, uint32_t *pData, uint32_t nOffset, uint32_t nLength, uint32_t options );
 }
 using namespace vex;
 
-// A class for the addressable led
-class AddressableLed {
-    public:
-        static const int32_t MAX_LEDS = 64;
+namespace vpi {
 
-    private:      
-        uint32_t maxled = MAX_LEDS;
-        uint32_t ledbuffer[MAX_LEDS];
-        uint32_t tmpbuffer[MAX_LEDS];
-        int32_t _index;
-        int32_t _id;
-  
-    public:
-      AddressableLed( triport::port &port, int32_t max = MAX_LEDS ) {
-        port.type( triportType::digitalOutput );
-        _index = port.index();
-        _id = port.id();
-        maxled = max <= MAX_LEDS ? max : MAX_LEDS;
-      }
+  // A class for the addressable led
+  class AddressableLed {
+      public:
+          static const int32_t MAX_LEDS = 64;
 
-      ~AddressableLed(){}
-
-      void Clear( color col = color(0x000000) ) {
-        for(int i=0;i<maxled;i++) {
-          ledbuffer[i] = col;
+      private:      
+          uint32_t maxled = MAX_LEDS;
+          uint32_t ledbuffer[MAX_LEDS];
+          uint32_t tmpbuffer[MAX_LEDS];
+          int32_t _index;
+          int32_t _id;
+    
+      public:
+        AddressableLed( triport::port &port, int32_t max = MAX_LEDS ) {
+          port.type( triportType::digitalOutput );
+          _index = port.index();
+          _id = port.id();
+          maxled = max <= MAX_LEDS ? max : MAX_LEDS;
         }
-        Set( ledbuffer, 0, maxled, 0);
-      }
 
-      void Set( uint32_t *pData, uint32_t nOffset, uint32_t nLength, uint32_t options ) {
-        vexAdiAddrLedSet( _index, _id, pData, nOffset, nLength, options );
+        ~AddressableLed(){}
 
-        // make copy if different buffer
-        if( pData != ledbuffer ) {
-          uint32_t *p = pData;
-          for(int i=nOffset;i<nLength && i<maxled;i++)
-            ledbuffer[i] = *p++;
-        }
-      }
-
-      void Set( color col = color(0x000000) ) {
-        for(int i=0;i<maxled;i++) {
-          ledbuffer[i] = col;
-        }
-      }
-
-      void Rotate( int n ) {
-          if( abs(n) >= maxled )
-            return;
-
-          if( n > 0 ) {
-            // make a copy
-            memcpy( tmpbuffer, ledbuffer, sizeof(ledbuffer));
-            memcpy( &ledbuffer[n], &tmpbuffer[0], (maxled-n)*sizeof(uint32_t) );
-            memcpy( &ledbuffer[0], &tmpbuffer[maxled-1-n], (n)*sizeof(uint32_t) );          
+        void Clear( color col = color(0x000000) ) {
+          for(int i=0;i<maxled;i++) {
+            ledbuffer[i] = col;
           }
-          else {
-            n = -n;
-            memcpy( tmpbuffer, ledbuffer, sizeof(ledbuffer));
-            memcpy( &ledbuffer[0], &tmpbuffer[n], (maxled-n)*sizeof(uint32_t) );
-            memcpy( &ledbuffer[maxled-n], &tmpbuffer[0], (n)*sizeof(uint32_t) );          
+          Set( ledbuffer, 0, maxled, 0);
+        }
+
+        void Set( uint32_t *pData, uint32_t nOffset, uint32_t nLength, uint32_t options ) {
+          vexAdiAddrLedSet( _index, _id, pData, nOffset, nLength, options );
+
+          // make copy if different buffer
+          if( pData != ledbuffer ) {
+            uint32_t *p = pData;
+            for(int i=nOffset;i<nLength && i<maxled;i++)
+              ledbuffer[i] = *p++;
           }
-      }
+        }
 
-      void Flush() {
-        Set( ledbuffer, 0, maxled, 0);
-      }
+        void Set( color col = color(0x000000) ) {
+          for(int i=0;i<maxled;i++) {
+            ledbuffer[i] = col;
+          }
+        }
 
-      int32_t Max() {
-        return maxled;
-      }
+        void Rotate( int n ) {
+            if( std::abs(n) >= maxled )
+              return;
+
+            if( n > 0 ) {
+              // make a copy
+              memcpy( tmpbuffer, ledbuffer, sizeof(ledbuffer));
+              memcpy( &ledbuffer[n], &tmpbuffer[0], (maxled-n)*sizeof(uint32_t) );
+              memcpy( &ledbuffer[0], &tmpbuffer[maxled-1-n], (n)*sizeof(uint32_t) );          
+            }
+            else {
+              n = -n;
+              memcpy( tmpbuffer, ledbuffer, sizeof(ledbuffer));
+              memcpy( &ledbuffer[0], &tmpbuffer[n], (maxled-n)*sizeof(uint32_t) );
+              memcpy( &ledbuffer[maxled-n], &tmpbuffer[0], (n)*sizeof(uint32_t) );          
+            }
+        }
+
+        void Flush() {
+          Set( ledbuffer, 0, maxled, 0);
+        }
+
+        int32_t Max() {
+          return maxled;
+        }
+  };
 };
